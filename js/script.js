2bc2bb999c303d655163ca15b9e0b2f724d78619
saraclaeys <sara.claeys01@gmail.com>
2020-04-22 12:37:45 +0200

    function equal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 function insert(num) {
-    document.forms.textview.value = document.form.textview.value + num
+    document.form.textview.value = document.form.textview.value + num
+}
+
+function equal() {
+    var exp = document.form.textview.value;
+    if (exp) {
+        document.form.textview.value = eval(exp)
+    }
 }
